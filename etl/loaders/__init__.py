b1f3444bdde9becfd5f997171856fab4c045aea6
1kimnet <kimnet.jensen@gmail.com>
2025-06-10 22:45:15 +0200

    Update sources.yaml to disable incompatible WFS service and clarify handler requirements; refactor __init__.py to remove unused FileGDBCoordinator import

--- etl/loaders/__init__.py
+++ etl/loaders/__init__.py
@@ -1,7 +1,6 @@
 """Re-export loaders for easy import."""
 
 from .filegdb import ArcPyFileGDBLoader  # noqa: F401
-from .filegdb_coordinator import FileGDBCoordinator  # noqa: F401  
 from .sde_loader import SDELoader  # noqa: F401
 
-__all__ = ["ArcPyFileGDBLoader", "FileGDBCoordinator", "SDELoader"]
+__all__ = ["ArcPyFileGDBLoader", "SDELoader"]
