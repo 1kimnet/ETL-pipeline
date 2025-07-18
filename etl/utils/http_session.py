e57f468976a57ca55636f4fc821d3f0a36d2852b
Jensen Kim <kimnet.jensen@gmail.com>
2025-07-18 14:09:19 +0200

    fix: Update import statement for Retry from urllib3 to improve compatibility

--- etl/utils/http_session.py
+++ etl/utils/http_session.py
@@ -9,7 +9,7 @@ from urllib.parse import urlparse
 
 import requests
 from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
+from urllib3.util.retry import Retry
 
 log = logging.getLogger(__name__)
 
